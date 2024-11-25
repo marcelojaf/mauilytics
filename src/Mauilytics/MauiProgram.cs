820903284da27565a04591b480072d172b977e69
Marcelo Amador <marcelojaf@gmail.com>
2024-11-25 14:49:29 -0300

    Analytics working

--- src/Mauilytics/MauiProgram.cs
+++ src/Mauilytics/MauiProgram.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+#if IOS
+using Plugin.Firebase.Core.Platforms.iOS;
+#elif ANDROID
+using Plugin.Firebase.Core.Platforms.Android;
+#endif
 
 namespace Mauilytics;
 
@@ -13,12 +19,30 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+			.RegisterFirebaseServices();
 
 #if DEBUG
 		builder.Logging.AddDebug();
 #endif
 
 		return builder.Build();
+	}
+
+	private static MauiAppBuilder RegisterFirebaseServices(this MauiAppBuilder builder)
+	{
+		builder.ConfigureLifecycleEvents(events => {
+#if IOS
+		events.AddiOS(iOS => iOS.WillFinishLaunching((_,__) => {
+                CrossFirebase.Initialize();
+                return false;
+		}));
+#elif ANDROID
+		events.AddAndroid(android => android.OnCreate((activity, _) =>
+						CrossFirebase.Initialize(activity)));
+#endif
+		});
+
+		return builder;
 	}
 }
